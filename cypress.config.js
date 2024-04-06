751faca1fa421a683d44d6af122aaa967dac16d1
Filipe Conde <filipecondep@gmail.com>
2024-04-05 21:16:21 -0300

    configure QASE on project

--- cypress.config.js
+++ cypress.config.js
@@ -5,8 +5,16 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://webdriveruniversity.com/To-Do-List/',
     watchForFileChanges: false,
+    video: false,
+    screenshotOnRunFailure: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-  },
+    reporter: 'cypress-qase-reporter',
+    reporterOptions: {
+      apiToken: '5578e5c67cba5da3d54e51c2b5194523a2801bf1443b7f79ebdcb03f999fa730',
+      projectCode: 'WUS',
+      logging: true,
+    },
+  }
 });
